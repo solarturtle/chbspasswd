990e01e118ec4a710e3c0a3ca6bd1ff2f3cca2c5
Charles H. Leggett <charles.leggett@oit.gatech.edu>
2013-12-11 07:23:24 -0500

    Added comments to stub out the planned options.

--- src/chbspasswd.cc
+++ src/chbspasswd.cc
@@ -1,6 +1,7 @@
-// chbspasswd
+// chbspasswd v0.1
 // 
-// A password generator inspired by XKCD 936: Password Strength
+// A password generator inspired by XKCD 936: Password Strength and
+// xkpasswd by Bart Busschots
 // 
 // Copyright (C) 2013-2014, Charles H. Leggett
 // 
@@ -20,44 +21,57 @@
 #include <iostream>
 #include <GetOpt.h>
 
+int showHelp();
+int showVersion();
 int chbs();
 
 int main(int argc, char **argv) {
 
   int flag;
-  while ((flag = getopt(argc, argv, "a:b:c:e:h:i:l:m:o:n:p:s:u:v:w:x")) != EOF) {
+
+  while ((flag = getopt(argc, argv, "a:b:c:e:hi:l:n:s:u:vw:x")) != EOF) {
     switch (flag) {
       case 'a':
+        // after - Add string of digits, special characters, or a combination to the end of the password.
+        std::cout << optarg << std::endl;
         break;
       case 'b':
+        // before - Add string of digits, special characters, or a combination to the beginning of the password.
         break;
       case 'c':
+        // case - Modify the words to be upper, lower, initial, or mixed case 
         break;
       case 'e':
+        // eleet - Make 1337sp3@k substitutions: a=@, e=3, i=!, l=1, o=0, and t=7.
         break;
       case 'h':
+        // help - Display a message with usage information 
+        showHelp();
         break;
       case 'i':
+        // inside - Add string of digits, special characters, or a combination between the words inside the password.
         break;
       case 'l':
-        break;
-      case 'm':
+        // length - Set minimum and maximum word length.
         break;
       case 'n':
-        break;
-      case 'o':
-        break;
-      case 'p':
+        // number - Set number of passwords to create.
         break;
       case 's':
+        // seperator - Set the preferences and count of seperator characters between words and other strings.
         break;
       case 'u':
+        // use - Specify a configuration file to use instead of the default .chbspasswdrc
         break;
       case 'v':
+        // version - Display a message with version information 
+        showVersion();
         break;
       case 'w':
+        // words - Set the number of words to use.
         break;
       case 'x':
+        // xkcd - Override all other options and return a well known password that you already have memorized. ;-)
         chbs();
         break;
       case '?':
@@ -68,13 +82,23 @@ int main(int argc, char **argv) {
   return 0;
 }
 
+int showHelp() {
+
+  std::cout << "Here is a helpful message." << std::endl;
+
+  return 0;
+}
+
+int showVersion() {
+
+  std::cout << "chbspassed v0.1" << std::endl;
+
+  return 0;
+}
+
 int chbs() {
-  std::string password;
 
-  password = "CorrectHorseBatteryStaple";
-
-  std::cout << password;
-  std::cout << std::endl;
+  std::cout << "CorrectHorseBatteryStaple" << std::endl;
 
   return 0;
 }
