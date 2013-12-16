2534ba48885eeecc1e68ce427ad16c0221dcefdb
Charles H. Leggett <charles.leggett@oit.gatech.edu>
2013-12-16 03:40:21 -0500

    Fleshing out the CHBSPassword class variables and adding comments.

--- src/chbspasswd.cc
+++ src/chbspasswd.cc
@@ -36,20 +36,34 @@ class CHBSPassword {
     void enableEleet();
 
   private:
-    std::string validSpecialCharacters;
-    std::string validSeperators;
+    std::string  validSpecialCharacters;
+    std::string  validSeperators;
 
-    bool        seperatorEnabled;
-    std::string seperator;
+    std::string  getWord();
+    int          wordCount;
+    int          wordMinLength;
+    int          wordMaxLength;
 
-    bool        beforeEnabled;
-    std::string before;
+    enum class   caseType {UPPER, LOWER, INITIAL, MIXED};
 
-    bool        afterEnabled;
-    std::string after;
+    std::string  getSeperator();
+    bool         separatorEnabled;
+    std::string  separatorCharacterCount;
+    std::string  separatorSaved;
+    enum class   separatorType {SAME, MIXED};
 
-    bool        insideEnabled;
-    std::string inside;
+    std::string  getBefore();
+    bool         beforeEnabled;
+    enum class   beforeType {DIGITS, SPECIAL, MIXED};
+
+    std::string  getInside();
+    bool         insideEnabled;
+    enum class   insideType {DIGITS, SPECIAL, MIXED};
+
+    std::string  getAfter();
+    bool         afterEnabled;
+    enum class   afterType {DIGITS, SPECIAL, MIXED};
+
 };
 
 int showHelp();
@@ -58,9 +72,9 @@ int showChbs();
 
 int main(int argc, char **argv) {
 
-  int flag;
-
   CHBSPassword thisPassword;
+
+  int flag;
 
   while ((flag = getopt(argc, argv, "a:b:c:e:hi:l:n:s:u:vw:x")) != EOF) {
     switch (flag) {
@@ -125,46 +139,59 @@ int main(int argc, char **argv) {
 
 CHBSPassword::CHBSPassword() {
 
+  wordCount = 3;
+  separatorEnabled = true;
+  // figure out how to use the separatorType enum. 
+  separatorCharacterCount = 1;
+
 }
 
 std::string CHBSPassword::getPassword(){
-  std::string password;
-  return password;
+  // Build and return password based on the defaults in the configuration file and modifying switches.
 }
 
 void CHBSPassword::setWordCount(){
+  // words - Set the number of words to use.
 
 }
 
 void CHBSPassword::setWordLength(){
+  // length - Set minimum and maximum word length.
 
 }
 
 void CHBSPassword::setWordCase(){
+  // case - Modify the words to be upper, lower, initial, or mixed case 
 
 }
 
 void CHBSPassword::setSeperator(){
+  // seperator - Set the preferences and count of seperator characters between words and other strings.
 
 }
 
 void CHBSPassword::setBefore(){
+  // before - Add string of digits, special characters, or a combination to the beginning of the password.
 
 }
 
 void CHBSPassword::setAfter(){
+  // after - Add string of digits, special characters, or a combination to the end of the password.
 
 }
 
 void CHBSPassword::setInside(){
+  // inside - Add string of digits, special characters, or a combination between the words inside the password.
 
 }
 
 void CHBSPassword::enableEleet(){
+  // eleet - Make 1337sp3@k substitutions: a=@, e=3, i=!, l=1, o=0, and t=7.
 
 }
 
 int showHelp() {
+  // help - Display a message with usage information 
 
   std::cout << "Here is a helpful message." << std::endl;
 
@@ -172,6 +199,7 @@ int showHelp() {
 }
 
 int showVersion() {
+  // version - Display a message with version information 
 
   std::cout << "chbspassed v0.1" << std::endl;
 
@@ -179,8 +207,10 @@ int showVersion() {
 }
 
 int showChbs() {
+  // xkcd - Override all other options and return a well known password that you have already memorized. ;-)
 
   std::cout << "CorrectHorseBatteryStaple" << std::endl;
 
   return 0;
 }
+
