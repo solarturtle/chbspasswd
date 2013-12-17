3a3843508566c966d7d529134d3dc403cd5a56ba
Charles H. Leggett <charles.leggett@oit.gatech.edu>
2013-12-17 07:24:02 -0500

    ajor changes that deserved many commits. I was being lazy while jumping around and trouble shooting. ;-)
    The highlights are:
    Moved to if/else instead of switch and began implementation of -b, -a,
    and -i options which all call the same subroutines.

--- src/chbspasswd.cc
+++ src/chbspasswd.cc
@@ -18,51 +18,55 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 #include <GetOpt.h>
 
 class CHBSPassword {
 
   public:
     CHBSPassword();
+    
     std::string getPassword();
-    void setWordCount();
-    void setWordLength();
-    void setWordCase();
-    void setSeperator();
-    void setBefore();
-    void setAfter();
-    void setInside();
-    void enableEleet();
+
+    void         setWordCount();
+    void         setWordLength();
+    void         setWordCase();
+    void         setSeparator();
+    bool         validatePadType ( std::string );
+    bool         validatePadCount ( std::string );
+    void         setBefore ( std::string, std::string );
+    void         setAfter ( std::string, std::string );
+    void         setInside ( std::string, std::string );
+    void         enableEleet();
 
   private:
     std::string  validSpecialCharacters;
-    std::string  validSeperators;
+    std::string  validSeparators;
 
     std::string  getWord();
     int          wordCount;
     int          wordMinLength;
     int          wordMaxLength;
+    std::string  wordCase();
 
-    enum class   caseType {UPPER, LOWER, INITIAL, MIXED};
+    std::string  getSeparator();
 
-    std::string  getSeperator();
-    bool         separatorEnabled;
-    std::string  separatorCharacterCount;
-    std::string  separatorSaved;
-    enum class   separatorType {SAME, MIXED};
+    std::string  getPadString ( std::string, std::string );
+    int          padMaxLength;
 
-    std::string  getBefore();
-    bool         beforeEnabled;
-    enum class   beforeType {DIGITS, SPECIAL, MIXED};
+    std::string  getBeforeString();
+    std::string  beforeType;
+    int          beforeCount;
 
-    std::string  getInside();
-    bool         insideEnabled;
-    enum class   insideType {DIGITS, SPECIAL, MIXED};
+    std::string  getAfterString();
+    std::string  afterType;
+    int          afterCount;
 
-    std::string  getAfter();
-    bool         afterEnabled;
-    enum class   afterType {DIGITS, SPECIAL, MIXED};
+    std::string  getInsideString();
+    std::string  insideType;
+    int          insideCount;
 
 };
 
@@ -70,79 +74,86 @@ int showHelp();
 int showVersion();
 int showChbs();
 
+std::vector<std::string> tokenize(std::string, char);
+
 int main(int argc, char **argv) {
 
   CHBSPassword thisPassword;
 
   int flag;
+  std::vector<std::string> arguments;
 
   while ((flag = getopt(argc, argv, "a:b:c:e:hi:l:n:s:u:vw:x")) != EOF) {
-    switch (flag) {
-      case 'a':
-        // after - Add string of digits, special characters, or a combination to the end of the password.
-        std::cout << optarg << std::endl;
-        thisPassword.setAfter();
-        break;
-      case 'b':
-        // before - Add string of digits, special characters, or a combination to the beginning of the password.
-        thisPassword.setBefore();
-        break;
-      case 'c':
-        // case - Modify the words to be upper, lower, initial, or mixed case 
-        thisPassword.setWordCase();
-        break;
-      case 'e':
-        // eleet - Make 1337sp3@k substitutions: a=@, e=3, i=!, l=1, o=0, and t=7.
-        thisPassword.enableEleet();
-        break;
-      case 'h':
-        // help - Display a message with usage information 
-        showHelp();
-        break;
-      case 'i':
-        // inside - Add string of digits, special characters, or a combination between the words inside the password.
-        thisPassword.setInside();
-        break;
-      case 'l':
-        // length - Set minimum and maximum word length.
-        thisPassword.setWordLength();
-        break;
-      case 'n':
-        // number - Set number of passwords to create.
-        break;
-      case 's':
-        // seperator - Set the preferences and count of seperator characters between words and other strings.
-        thisPassword.setSeperator();
-        break;
-      case 'u':
-        // use - Specify a configuration file to use instead of the default .chbspasswdrc
-        break;
-      case 'v':
-        // version - Display a message with version information 
-        showVersion();
-        break;
-      case 'w':
-        // words - Set the number of words to use.
-        thisPassword.setWordCount();
-        break;
-      case 'x':
-        // xkcd - Override all other options and return a well known password that you have already memorized. ;-)
-        showChbs();
-        break;
-      case '?':
-        break;
+    if ( flag == 'a' ) {
+      // after - Add string of digits, special characters, or a combination to the end of the password.
+      arguments = tokenize(optarg, ',');
+//      if ( thisPassword.validatePadType (arguments[0]) ) {
+//        std::cout << "HERE" << std::endl; // debug message
+//      }
+    }
+    else if ( flag == 'b' ) {
+      // before - Add string of digits, special characters, or a combination to the beginning of the password.
+      arguments = tokenize(optarg, ',');
+      thisPassword.setBefore ( arguments[0], arguments[1] );
+    }
+    else if ( flag == 'c' ) {
+      // case - Modify the words to be upper, lower, initial, or mixed case 
+      thisPassword.setWordCase();
+    }
+    else if ( flag == 'e' ) {
+      // eleet - Make 1337sp3@k substitutions: a=@, e=3, i=!, l=1, o=0, and t=7.
+      thisPassword.enableEleet();
+    }
+    else if ( flag == 'h' ) {
+      // help - Display a message with usage information 
+      showHelp();
+      return 0;
+    }
+    else if ( flag == 'i' ) {
+      // inside - Add string of digits, special characters, or a combination between the words inside the password.
+      arguments = tokenize(optarg, ',');
+      thisPassword.setInside ( arguments[0], arguments[1] );
+    }
+    else if ( flag == 'l' ) {
+      // length - Set minimum and maximum word length.
+      thisPassword.setWordLength();
+    }
+    else if ( flag == 'n' ) {
+      // number - Set number of passwords to create.
+    }
+    else if ( flag == 's' ) {
+      // seperator - Set the preferences and count of seperator characters between words and other strings.
+      thisPassword.setSeparator();
+    }
+    else if ( flag == 'u' ) {
+      // use - Specify a configuration file to use instead of the default .chbspasswdrc
+    }
+    else if ( flag == 'v' ) {
+      // version - Display a message with version information 
+      showVersion();
+      return 0;
+    }
+    else if ( flag == 'w' ) {
+      // words - Set the number of words to use.
+      thisPassword.setWordCount();
+    }
+    else if ( flag == 'x' ) {
+      // xkcd - Override all other options and return a well known password that you have already memorized. ;-)
+      showChbs();
+      return 0;
     }
   }
-
-  return 0;
 }
+
+/////////////////////////////////////////////////////////////////////////////////
+// BEGIN CHBSPassword Public
 
 CHBSPassword::CHBSPassword() {
 
   wordCount = 3;
-  separatorEnabled = true;
-  // figure out how to use the separatorType enum. 
-  separatorCharacterCount = 1;
+  //separatorEnabled = true;
+  //separatorCharacterCount = 1;
+  padMaxLength = 5;
 
 }
 
@@ -151,44 +162,132 @@ std::string CHBSPassword::getPassword(){
 }
 
 void CHBSPassword::setWordCount(){
+  // Set variables for ...
   // words - Set the number of words to use.
 
 }
 
 void CHBSPassword::setWordLength(){
+  // Set variables for ...
   // length - Set minimum and maximum word length.
 
 }
 
 void CHBSPassword::setWordCase(){
+  // Set variables for ...
   // case - Modify the words to be upper, lower, initial, or mixed case 
 
 }
 
-void CHBSPassword::setSeperator(){
+void CHBSPassword::setSeparator(){
+  // Set variables for ...
   // seperator - Set the preferences and count of seperator characters between words and other strings.
 
 }
 
-void CHBSPassword::setBefore(){
-  // before - Add string of digits, special characters, or a combination to the beginning of the password.
+bool validatePadType ( std::string padType ) {
+
+  std::transform(padType.begin(), padType.end(), padType.begin(), ::tolower);
+
+  if ( padType == "d" ) {
+    // Using Pad of Digits
+    std::cout << "DIGITS" << std::endl; // debug message
+    
+    return true;
+  }
+  else if ( padType == "s" ) {
+    // Using Pad of Special Characters
+    std::cout << "SPECIAL" << std::endl; // debug message
+    
+    return true;
+  }
+  else if ( padType == "m" ) {
+    // Using Pad of Mixed Digits and Special Characters
+    std::cout << "MIXED" << std::endl; // debug message
+    
+    return true;
+  }
+  else {
+    // invalid argument
+    std::cout << "./chbspasswd: unexpected argument -- " << padType << std::endl;
+    showHelp();
+
+    return false;
+  }
+
+  std::cout << "HERE" << std::endl; // debug message
+}
+
+bool validatePadCount ( int padCount, int padMaxLength ) {
+
+  if ( padCount <= padMaxLength ) {
+    return true;
+  }
+  else {
+    return false;
+  }
 
 }
 
-void CHBSPassword::setAfter(){
+void CHBSPassword::setBefore ( std::string beforeType, std::string beforeCount) {
+  // Set variables for ...
+  // before - Add string of digits, special characters, or a combination to the beginning of the password.
+
+
+}
+
+void CHBSPassword::setAfter ( std::string afterType, std::string afterCount ) {
+  // Set variables for ...
   // after - Add string of digits, special characters, or a combination to the end of the password.
 
 }
 
-void CHBSPassword::setInside(){
+void CHBSPassword::setInside ( std::string insideType, std::string insideCount) {
+  // Set variables for ...
   // inside - Add string of digits, special characters, or a combination between the words inside the password.
 
 }
 
 void CHBSPassword::enableEleet(){
+  // Set variables for ...
   // eleet - Make 1337sp3@k substitutions: a=@, e=3, i=!, l=1, o=0, and t=7.
 
 }
+
+// END CHBSPassword Public
+/////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////
+// BEGIN CHBSPassword Private
+
+std::string CHBSPassword::getBeforeString() {
+  // Return string for ...
+  // before - Add string of digits, special characters, or a combination to the beginning of the password.
+  
+}
+
+std::string CHBSPassword::getAfterString() {
+  // Return string for ...
+  // after - Add string of digits, special characters, or a combination to the end of the password.
+
+}
+
+std::string CHBSPassword::getInsideString() {
+  // Return string for ...
+  // inside - Add string of digits, special characters, or a combination between the words inside the password.
+
+}
+
+std::string CHBSPassword::getPadString ( std::string padType, std::string count ){
+  // Called by get{Before,After,Inside} to use variables to build and return string.
+  
+}
+
+// END CHBSPassword Private
+/////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////
+// BEGIN Main Functions
 
 int showHelp() {
   // help - Display a message with usage information 
@@ -201,7 +300,7 @@ int showHelp() {
 int showVersion() {
   // version - Display a message with version information 
 
-  std::cout << "chbspassed v0.1" << std::endl;
+  std::cout << "chbspasswd v0.1" << std::endl;
 
   return 0;
 }
@@ -213,4 +312,22 @@ int showChbs() {
 
   return 0;
 }
+
+std::vector<std::string> tokenize(std::string delimiterSeparatedString, char delimiter) {
+  
+  std::vector<std::string> tokens;
+    
+  std::istringstream iss(delimiterSeparatedString);
+  std::string token;
+
+  while (std::getline(iss, token, delimiter)) {
+    tokens.push_back(token);
+  }
+
+  return tokens;
+
+};
+
+// END Main Functions
+/////////////////////////////////////////////////////////////////////////////////
 
